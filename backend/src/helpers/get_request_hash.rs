4e31c2e521c4b1c124367875f71bb97041b8acb0
PhillY <philly706869@gmail.com>
2025-12-16 12:03:58 +0000

    get_request_hash to_hex 명시

--- backend/src/helpers/get_request_hash.rs
+++ backend/src/helpers/get_request_hash.rs
@@ -4,5 +4,5 @@ use crate::helpers::get_x_real_ip;
 
 pub fn get_request_hash(req: &HttpRequest) -> actix_web::Result<String> {
     let ip = get_x_real_ip(req)?;
-    Ok(blake3::hash(ip).to_string())
+    Ok(blake3::hash(ip).to_hex().to_string())
 }
