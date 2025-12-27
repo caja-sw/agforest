86c565a4b7b4ebad37def822c7999c58856edb5c
PhillY <philly706869@gmail.com>
2025-12-27 17:13:51 +0000

    2025-12-28

--- frontend/src/lib/api/util.js
+++ frontend/src/lib/api/util.js
@@ -3,7 +3,7 @@ import { env } from "$env/dynamic/public";
 
 const API_BASE = browser
   ? env.PUBLIC_AGFOREST_EXTERNAL_API_URL
-  : (await import("$env/dynamic/private")).env.AGFOREST_INTERNAL_API_URL;
+  : env.PUBLIC_AGFOREST_INTERNAL_API_URL;
 
 /** @param {string} route */
 export const resolveAPI = (route) => new URL(route, API_BASE);
