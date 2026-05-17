04002975d82622ed0cee0f46160e5dd1680060dd
PhillY <philly706869@gmail.com>
2026-05-17 10:34:34 +0000

    의존성 업데이트

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,27 +1,28 @@
-import { includeIgnoreFile } from "@eslint/compat";
+import { includeIgnoreFile } from "@eslint/config-helpers";
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
-import { fileURLToPath } from "node:url";
+import path from "node:path";
 import svelteConfig from "./svelte.config.js";
 
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default defineConfig(
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
-  ...svelte.configs.recommended,
+  svelte.configs.recommended,
   prettier,
-  ...svelte.configs.prettier,
+  svelte.configs.prettier,
   {
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
     },
+    rules: { "no-undef": "off" },
   },
   {
     files: ["**/*.svelte", "**/*.svelte.js"],
     languageOptions: { parserOptions: { svelteConfig } },
   },
-];
+);
